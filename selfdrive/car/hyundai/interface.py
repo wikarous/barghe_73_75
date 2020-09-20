a030fb38cc66e9a013db587dbb558620c97b9e22
unknown <64267486@naver.com>
2020-09-20 21:20:34 +0900

    openpilot v0.7.3 PID tuning

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -56,10 +56,10 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kf = 0.00006
       ret.mass = 1685. + STD_CARGO_KG
       ret.wheelbase = 2.855
-      ret.steerRatio = 13.0   #Spec
+      ret.steerRatio = 12.7   #Spec
       tire_stiffness_factor = 0.385
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.07], [0.04]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.08], [0.04]]
       ret.minSteerSpeed = -0 * CV.MPH_TO_MS
     elif candidate in [CAR.KIA_K7_HEV]:
       ret.lateralTuning.pid.kf = 0.00005
